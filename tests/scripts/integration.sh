bfce90bc304d1c99c8ce1988b0de246c1fd3b4a2
Jörn Ott <joern.ott@ott-consult.de>
2024-07-12 09:29:59 +0200

    Fix unit/integration test scripts

--- tests/scripts/integration.sh
+++ tests/scripts/integration.sh
@@ -1,7 +1,67 @@
 #!/bin/bash
 set -e
-vendor/bin/phpunit \
-    -c phpunit.xml \
+export XDEBUG_MODE=coverage
+PHPUNIT="vendor/bin/phpunit"
+if [ ! -f "${PHPUNIT}" ]; then
+    PHPUNIT="/var/www/${PHPUNIT}"
+    if [ ! -f "${PHPUNIT}" ]; then
+        echo -e "\033[0;31mCould not find phpunit in vendor/bin or /var/www/vendor/bin\033[0m"
+        exit 1
+    fi
+fi
+BOOTSTRAP="../../../tests/bootstrap.php"
+if [ ! -f "${BOOTSTRAP}" ]; then
+    BOOTSTRAP="../oxideshop-ce/tests"
+    if [ ! -f "${BOOTSTRAP}" ]; then
+        echo -e "\033[0;31mCould not find bootstrap.php in ../../../tests or ../oxideshop-ce/tests\033[0m"
+        exit 1
+    fi
+fi
+"${PHPUNIT}" \
+    -c tests/phpunit.xml \
+    --bootstrap "${BOOTSTRAP}" \
     --coverage-clover=tests/Reports/coverage_phpunit_integration.xml \
     tests/Integration 2>&1 \
 | tee tests/Output/integration_tests.txt
+RESULT=$?
+echo "phpunit exited with error code ${RESULT}"
+if [ ! -s "tests/Output/integration_tests.txt" ]; then
+    echo -e "\033[0;31mLog file is empty! Seems like no tests have been run!\033[0m"
+    RESULT=1
+fi
+cat >failure_pattern.tmp <<EOF
+fail
+\\.\\=\\=
+Warning
+Notice
+Deprecated
+Fatal
+Error
+DID NOT FINISH
+Test file ".+" not found
+Cannot open file
+No tests executed
+Could not read
+Warnings: [1-9][0-9]*
+Errors: [1-9][0-9]*
+Failed: [1-9][0-9]*
+Deprecations: [1-9][0-9]*
+Risky: [1-9][0-9]*
+EOF
+sed -e 's|(.*)\r|$1|' -i failure_pattern.tmp
+while read -r LINE ; do
+    if [ -n "${LINE}" ]; then
+        if grep -q -E "${LINE}" "tests/Output/integration_tests.txt"; then
+            echo -e "\033[0;31m integration test failed matching pattern ${LINE}\033[0m"
+            grep -E "${LINE}" "tests/Output/integration_tests.txt"
+            RESULT=1
+        else
+            echo -e "\033[0;32m integration test passed matching pattern ${LINE}"
+        fi
+    fi
+done <failure_pattern.tmp
+if [[ ! -s "tests/Reports/coverage_phpunit_integration.xml" ]]; then
+    echo -e "\033[0;31m coverage report tests/Reports/coverage_phpunit_integration.xml is empty\033[0m"
+    RESULT=1
+fi
+exit ${RESULT}
